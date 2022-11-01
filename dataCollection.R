73a77e3bceff8a53c3d76d19e6e1502324f4b4ff
jDobkin <78820023+jDobkin@users.noreply.github.com>
2022-11-01 15:11:57 -0400

    include dvrpc calculated fields

--- dataCollection.R
+++ dataCollection.R
@@ -3,8 +3,8 @@ getwd()
 vars2020 <- load_variables(2020, "acs5")
 
 #census keys
-census_api_key("insert key", overwrite = TRUE)
-ckey <- "insert key"
+census_api_key("insert_api_key", overwrite = TRUE)
+ckey <- "insert_api_key"
 
 #grab that data!
 pumas <- get_acs(geography = "public use microdata area",
@@ -48,7 +48,14 @@ tracts <- tracts %>% rename_at(vars(oldnames), ~ newnames)
 pumas <- pumas %>% rename_at(vars(oldnames), ~ newnames)
 pumas2 <- pumas2 %>% rename_at(vars(oldnames), ~ newnames)
 
-
+#add 5% of total pop
+tracts$pop5pct_E <- (.05 * tracts$TT_POP_E)
+pumas$pop5pct_E <- (.05 * pumas$TT_POP_E)
+#pumas2$pop5pct_E <- (.05 * pumas2$TT_POP_E)
+tracts$TT_POP_LEP_E <- with(tracts, sum(Span_Lim_E, FRE_Lim_E, GER_Lim_E, RUS_Lim_E, IND_Lim_E, KOR_Lim_E,
+                               CHI_Lim_E, Viet_Lim_E, TAG_Lim_E, PAC_Li_E, ARB_Lim_E, OTH_Lim_E))
+pumas$TT_POP_LEP_E <- with(pumas, sum(Span_Lim_E, FRE_Lim_E, GER_Lim_E, RUS_Lim_E, IND_Lim_E, KOR_Lim_E,
+                                        CHI_Lim_E, Viet_Lim_E, TAG_Lim_E, PAC_Li_E, ARB_Lim_E, OTH_Lim_E))
 #export
 write_csv(pumas, here("pumasLEP.csv"))
 write_csv(tracts, here("tractsLEP.csv"))
